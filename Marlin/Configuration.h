685c24f152383afe55428cf1c2494b53a56b8758
Harm Berntsen <git@harmberntsen.nl>
2024-02-24 10:45:11 +0100

    Tune movement range
    With the v1 firmware, we had to set a Z offset in the menus and added an
    X, Y offset at the start of the gcode. With the new firmware, we can set
    a home offset for X, Y and Z at once. That makes things easier.
    
    Soft endstops are also enabled, which measure the distance from the home
    position to the other end of the movement range. That means that when
    you move an axis through the printer menu, it won't go further than
    mechanically possible. There is an option to disable the limit, for
    example to use a negative Z when configuring the home offsets.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1723,16 +1723,24 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 210
-#define Y_BED_SIZE 200
+#define X_BED_SIZE 180
+#define Y_BED_SIZE 140
+
+// Disable home offsets min/max
+#define DISABLE_M428_LIMITS
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -20  // Bart Venneker
+#define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 350 // Stel hier in wat de maximale hoogte is !!
+
+// Numbers are before setting home offsets, the maximum distance from the end
+// stop without the print head bumping into something.
+// Measured on Harm's printer
+#define X_MAX_POS 227
+#define Y_MAX_POS 198
+#define Z_MAX_POS 134
+
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
 //#define J_MIN_POS 0
@@ -1784,7 +1792,7 @@
 #endif
 
 #if EITHER(MIN_SOFTWARE_ENDSTOPS, MAX_SOFTWARE_ENDSTOPS)
-  //#define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
+  #define SOFT_ENDSTOPS_MENU_ITEM  // Enable/Disable software endstops from the LCD
 #endif
 
 /**
