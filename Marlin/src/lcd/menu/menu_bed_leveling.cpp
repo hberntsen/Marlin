hberntsen/Marlin

commit 16a14bac9975b802e62aa629f1da3406c726f14c

Only show home option for bed leveling when needed
In some situations you don't need to auto home manually. Remove the
option for those cases

Marlin/src/lcd/menu/menu_bed_leveling.cpp
@@ -243,13 +243,14 @@ void menu_bed_leveling() {
   START_MENU();
   BACK_ITEM(MSG_CONFIGURATION);
 
-  // Auto Home if not using manual probing
-  #if NONE(PROBE_MANUALLY, MESH_BED_LEVELING)
-    if (!is_homed) GCODES_ITEM(MSG_AUTO_HOME, FPSTR(G28_STR));
-  #endif
 
   // Level Bed
   #if ANY(PROBE_MANUALLY, MESH_BED_LEVELING)
+    // Auto Home if not using manual probing
+    #if NONE(PROBE_MANUALLY, MESH_BED_LEVELING) && !
+      if (!is_homed) GCODES_ITEM(MSG_AUTO_HOME, FPSTR(G28_STR));
+    #endif
+
     // Manual leveling uses a guided procedure
     SUBMENU(MSG_LEVEL_BED, _lcd_level_bed_continue);
   #else

commit 60296d9caddf21f98dd51f68a643562e5fa1587a

Fix typo in bed leveling menu
Issue was introduced in 16a14bac9975b802e62aa629f1da3406c726f14c

Marlin/src/lcd/menu/menu_bed_leveling.cpp
@@ -270,7 +270,7 @@ void menu_bed_leveling() {
   // Level Bed
   #if ANY(PROBE_MANUALLY, MESH_BED_LEVELING)
     // Auto Home if not using manual probing
-    #if NONE(PROBE_MANUALLY, MESH_BED_LEVELING) && !
+    #if NONE(PROBE_MANUALLY, MESH_BED_LEVELING)
       if (!is_homed) GCODES_ITEM(MSG_AUTO_HOME, FPSTR(G28_STR));
     #endif
 