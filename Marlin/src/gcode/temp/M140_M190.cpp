468e3c465ec54482e17520b058eca0556e8d221c
Harm Berntsen <git@harmberntsen.nl>
2025-11-08 14:56:21 +0000

    Fix MSG_BED_HEATING not shown on print start
    The print job timer autostart causes the status message to be
    overwritten. So swap the order around to actually show the bed
    heating/cooling message.

--- Marlin/src/gcode/temp/M140_M190.cpp
+++ Marlin/src/gcode/temp/M140_M190.cpp
@@ -98,13 +98,13 @@ void GcodeSuite::M140_M190(const bool isM190) {
     constexpr bool anneal = false;
   #endif
 
+  // With PRINTJOB_TIMER_AUTOSTART, M190 can start the timer, and M140 can stop it
+  TERN_(PRINTJOB_TIMER_AUTOSTART, thermalManager.auto_job_check_timer(isM190, !isM190));
+
   if (!anneal) {
     thermalManager.setTargetBed(temp);
     thermalManager.isHeatingBed() ? LCD_MESSAGE(MSG_BED_HEATING) : LCD_MESSAGE(MSG_BED_COOLING);
   }
-
-  // With PRINTJOB_TIMER_AUTOSTART, M190 can start the timer, and M140 can stop it
-  TERN_(PRINTJOB_TIMER_AUTOSTART, thermalManager.auto_job_check_timer(isM190, !isM190));
 
   if (isM190) {
     #if ENABLED(BED_ANNEALING_GCODE)
